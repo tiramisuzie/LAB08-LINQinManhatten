8dc5cce44d55c410bb930f8fe224de7ef0053a2d
suzanne su <suhysu@gmail.com>
2018-10-21 17:59:02 -0700

    Add project files.

---
+++ LAB08-LINQinManhatten/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LAB08_LINQinManhatten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
